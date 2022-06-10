5ac641ef895ca938689531ea472b0fdb39f0eb1e
Dylan Brito <dylan-brito@oultook.fr>
2022-06-10 12:24:50 +0200

    création du projet vide

---
+++ 13-06_Exercie1/MercedesAccros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_06_Exercie1
+{
+    class MercedesAccros : ITruck
+    {
+        public void LicencePlate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MaxFreight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MaxLegalSpead()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 13-06_Exercie1/RenaudClio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_06_Exercie1
+{
+    class RenaudClio : ICar
+    {
+        public void LicencePlate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MaxLegalSpead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MaxPassengerCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CarTemplate
+    }
+}
