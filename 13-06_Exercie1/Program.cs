342727f3936b736c7b1985969e9f9b4a685e205c
Dylan Brito <dylan-brito@oultook.fr>
2022-06-10 11:16:55 +0200

    création du projet vide

---
+++ 13-06_Exercie1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13_06_Exercie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
